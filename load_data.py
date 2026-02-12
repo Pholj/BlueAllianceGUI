59732688dbb97f25e4114cee3de7de02096599d5
Phol <43488814+Pholj@users.noreply.github.com>
2026-02-12 18:14:48 -0500

    initial loading of data, adding the cache

--- load_data.py
+++ load_data.py
@@ -1,4 +1,6 @@
 import requests
+import json
+from pathlib import Path
 
 def load_data(api_key, endpoint):
     baseURL = "https://www.thebluealliance.com/api/v3"
@@ -13,5 +15,21 @@ def load_data(api_key, endpoint):
     else:
         response.raise_for_status()
 
-def load_all_data(api_key, event):
+def load_all_data(api_key, event_key):
     #decide what you need to do
+
+    #Gets a list of Team keys that competed in the given event.
+    team_keys = load_data(api_key, f"/event/{event_key}/teams")
+    OPRS = load_data(api_key, f"/event/{event_key}/oprs")["oprs"]
+
+    save_to_json(OPRS, event_key)
+    return OPRS
+
+def fetch_data():
+    path = Path("data/2025mosl.json")
+
+
+def save_to_json(data, event_key):
+    Path("data").mkdir(exist_ok=True)
+    with open(f"data/{event_key}.json", "w") as f:
+        json.dump(data, f, indent=2)
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from load_data import load_all_data, save_to_json
 
 def main():
     #Get event name and store that event
-    #event = input("Enter the event name: ")
-    event = 
+    API_KEY = ""
+    event = "2025mosl"
+    print("hi")
+    print(load_all_data(API_KEY, event))
+
+main()
